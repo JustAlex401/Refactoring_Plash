bdb259f1a8dad5f1dbdc41e4ac005c36dac88e22
JustAlex401 <plashko99@mail.ru>
2020-09-30 16:13:24 +0300

    first commit

--- hw/hw-refactoring-code.ts
+++ hw/hw-refactoring-code.ts
@@ -2,55 +2,111 @@ import * as _ from "underscore";
 import { db, Widget, DataLinkKey } from "./db";
 const arrify = require('arrify');
 
+
+// The Context defines interface for clients. 
+export class Context {
+    private strategy : Strategy;
+
+    constructor(strategy: Strategy){
+        this.strategy = strategy;
+    }
+
+    public setStrategy (strategy: Strategy){
+        this.strategy = strategy;
+    }
+
+    public getStrategy (){
+        return this.strategy;
+    }
+
+     // The Context delegates some work to the Strategy object. 
+    public async doSomeBusinessLogic(widgetOrWidgets: Widget | Widget[]): Promise<Widget[]> {
+        return await this.strategy.doAlgorithm(widgetOrWidgets);
+    }
+}
+
+ // The Strategy interface declares operations common to all supported versions of some algorithm.
+interface Strategy {
+    doAlgorithm(widgetOrWidgets: Widget | Widget[]): Promise<Widget[]>;
+}
+
+
+// First traversal algorithm.
+export class StrategyA implements Strategy{
+
+    public async doAlgorithm(widgetOrWidgets: Widget | Widget[]): Promise<Widget[]> {
+
+        const widgets = arrify(widgetOrWidgets);
+        let loaded: Widget[] = [...widgets];
+
+        let toLoad: DataLinkKey[] = getDataLinkKeysToLoad(widgets);
+
+        // Limit maximum resolution depth
+        const DEPTH = 58;
+        for (let depth = 0; depth < DEPTH && toLoad.length; depth++) {
+
+            // Filter out widgets that has been already loaded
+            toLoad = filtration(toLoad, loaded);
+
+            // If nothing to resolve, that means that everything has been resolved
+            if (!toLoad.length) {
+                break;
+            }
+
+            let loadedChunk = await Promise.all(
+                toLoad.map(k => getWidget(k.tenantId, k.cardUuid, k.widgetUuid))
+            );
+
+            // Filter out not found widgets
+            const filteredChunk = loadedChunk.filter(x => x) as Widget[];
+
+            // Add to loaded array
+            loaded = [...loaded, ...filteredChunk];
+
+            toLoad = getDataLinkKeysToLoad(filteredChunk);
+        }
+
+            return loaded;
+        }
+
+}
+
+
+/* Second traversal algorithm. */
+
+/*
+export class StrategyB implements Strategy{
+    public async doAlgorithm(widgetOrWidgets: Widget | Widget[]): Promise<Widget[]> {
+
+    }
+}
+*/
+
+
+
 export function getWidget(tenantId: string, cardUuid: string, uuid: string): Promise<Widget | undefined> {
     return Promise.resolve(
         db.find(x => x.tenantId === tenantId && x.cardUuid === cardUuid && x.uuid === uuid)
     )
-}
-
-//FIXME: In the future there might be passed another traversal algorithm
-export async function getDataLinkWidgetsChain(widgetOrWidgets: Widget | Widget[]): Promise<Widget[]> {
-    const widgets = arrify(widgetOrWidgets);
-    let loaded: Widget[] = [...widgets];
-
-    const getDataLinkKeysToLoad = (chunk: Widget[]): DataLinkKey[] => _.flatten(
-        chunk.map(
-            w => _.values(
-                _.mapObject(w.dataLink || {}, val => val.widgetKey)
-            )
-        )
-    );
-
-    let toLoad: DataLinkKey[] = getDataLinkKeysToLoad(widgets);
-
-    //limit maximum resolution depth
-    for (let depth = 0; depth < 58 && toLoad.length; depth++) {
-        //filter out widgets that has been already loaded
-        toLoad = _.uniq(
-            toLoad.filter(
-                k => !loaded.find(w => k.cardUuid === w.cardUuid && w.uuid === k.widgetUuid)
-            ),
-            false,
-            x => `${x.tenantId}${x.cardUuid}${x.widgetUuid}`
-        );
-
-        //if nothing to resolve, that means that everything has been resolved
-        if (!toLoad.length) {
-            break;
-        }
-
-        let loadedChunk = await Promise.all(
-            toLoad.map(k => getWidget(k.tenantId, k.cardUuid, k.widgetUuid))
-        );
-
-        //filter out not found widgets
-        const filteredChunk = loadedChunk.filter(x => x) as Widget[];
-
-        //add to loaded array
-        loaded = [...loaded, ...filteredChunk];
-
-        toLoad = getDataLinkKeysToLoad(filteredChunk);
     }
 
-    return loaded;
+
+const getDataLinkKeysToLoad = (chunk: Widget[]): DataLinkKey[] => _.flatten(
+    chunk.map(
+        w => _.values(
+            _.mapObject(w.dataLink || {}, val => val.widgetKey)
+        )
+    )
+)
+
+const filtration = (toLoad: DataLinkKey[], loaded: Widget[]) : DataLinkKey[] => {
+    toLoad = _.uniq(
+        toLoad.filter(
+            k => !loaded.find(w => k.cardUuid === w.cardUuid && w.uuid === k.widgetUuid)
+        ),
+        false,
+        x => `${x.tenantId}${x.cardUuid}${x.widgetUuid}`
+    );
+    return toLoad;
 }
+
--- hw/test.ts
+++ hw/test.ts
@@ -1,4 +1,7 @@
-import { getDataLinkWidgetsChain, getWidget } from "./hw-refactoring-code";
+// import { getDataLinkWidgetsChain, getWidget } from "./hw-refactoring-code";
+
+import {getWidget, Context, StrategyA} from "./hw-refactoring-code";
+
 import * as _ from "underscore";
 import { Widget } from "./db";
 const deepEqual = require("deep-equal");
@@ -11,7 +14,9 @@ async function test() {
         await getWidget("1", "1", "3") as Widget,
         await getWidget("1", "1", "4") as Widget
     ];
-    const res1 = await getDataLinkWidgetsChain(await getWidget("1", "1", "1") as Widget);
+
+    const context = new Context(new StrategyA());
+    const res1 = await context.doSomeBusinessLogic(await getWidget("1", "1", "1") as Widget);
 
     console.assert(
         deepEqual(_.sortBy(expected1, x => x.uuid), _.sortBy(res1, x => x.uuid)), 
@@ -21,7 +26,7 @@ async function test() {
     const expected2 = [
         await getWidget("1", "1", "7") as Widget
     ]
-    const res2 = await getDataLinkWidgetsChain(await getWidget("1", "1", "7") as Widget);
+    const res2 = await context.doSomeBusinessLogic(await getWidget("1", "1", "7") as Widget);
 
     console.assert(
         deepEqual(_.sortBy(expected2, x => x.uuid), _.sortBy(res2, x => x.uuid)), 
@@ -32,7 +37,7 @@ async function test() {
         await getWidget("1", "1", "5") as Widget,
         await getWidget("1", "1", "6") as Widget
     ]
-    const res3 = await getDataLinkWidgetsChain(await getWidget("1", "1", "6") as Widget);
+    const res3 = await context.doSomeBusinessLogic(await getWidget("1", "1", "6") as Widget);
 
     console.assert(
         deepEqual(_.sortBy(expected3, x => x.uuid), _.sortBy(res3, x => x.uuid)), 
